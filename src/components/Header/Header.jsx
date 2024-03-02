99d82d2d0e2fff3f1cb116e40021e3db99683d1a
Igor2109 <127054357+Igor2109@users.noreply.github.com>
2024-03-02 14:34:12 +0100

    routes-fixed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,4 @@
 import { HOME_ROUTE, SHOPPING_CART_ROUTE } from 'constants/routes';
-import ShoppingCart from 'pages/ShoppingCart/ShoppingCart';
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
